34e34d4261469874f1f75b62903998fc16210a4d
bdring <barton.dring@gmail.com>
2015-09-03 10:57:27 -0500

    Bug fix for multiple parking issue
    Multiple parks on the same move would put the controller into
    unresponsive state.

--- grbl.h
+++ grbl.h
@@ -22,8 +22,8 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.0.4"
-#define GRBL_VERSION_BUILD "20150901"
+#define GRBL_VERSION "1.0.5"
+#define GRBL_VERSION_BUILD "20150903"
 
 // Define standard libraries used by Grbl.
 #include <avr/io.h>
