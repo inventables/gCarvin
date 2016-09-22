2dbcc25afb7e8d334db387cda3995bd8c293d8c9
bdring <barton.dring@gmail.com>
2016-09-22 18:52:14 -0500

    Fixed rev info
    Forgot to update the firmware rev number in grbl.h

--- grbl.h
+++ grbl.h
@@ -22,8 +22,8 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.2.9"
-#define GRBL_VERSION_BUILD "20160906"
+#define GRBL_VERSION "1.2.10"
+#define GRBL_VERSION_BUILD "20160922"
 
 // Define standard libraries used by Grbl.
 #include <avr/io.h>
