fb2ed8a39097beaee680a81903afd665c91d0675
jandersonfrominventables <janderson@inventables.com>
2017-03-05 21:22:51 -0600

    Updated product revision to 2.0.0-alpha as this branch will contain features that are incompatible with previous version

--- grbl.h
+++ grbl.h
@@ -22,8 +22,8 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1.5" // changed defaults, LED levels and parking pullout speeds
-#define GRBL_VERSION_BUILD "20151216"
+#define GRBL_VERSION "2.0.0-alpha"
+#define GRBL_VERSION_BUILD "20170303"
 
 // Define standard libraries used by Grbl.
 #include <avr/io.h>
